f636cd09a0c3579e93c749f1924cf2ae09e658d7
schluckebier <schluckebierd@gmail.com>
2014-09-09 20:34:08 -0500

    Fixes #2

--- TestCollatz.py
+++ TestCollatz.py
@@ -36,19 +36,19 @@ class TestCollatz (TestCase) :
 
     def test_eval_1 (self) :
         v = collatz_eval(1, 10)
-        self.assertEqual(v, 1)
+        self.assertEqual(v, 2)
 
     def test_eval_2 (self) :
         v = collatz_eval(100, 200)
-        self.assertEqual(v, 1)
+        self.assertEqual(v, 125)
 
     def test_eval_3 (self) :
         v = collatz_eval(201, 210)
-        self.assertEqual(v, 1)
+        self.assertEqual(v, 89)
 
     def test_eval_4 (self) :
         v = collatz_eval(900, 1000)
-        self.assertEqual(v, 1)
+        self.assertEqual(v, 174)
 
     # -----
     # print
@@ -76,75 +76,23 @@ class TestCollatz (TestCase) :
 main()
 
 """
-% coverage3 run --branch TestCollatz.py
-FFFF..F
-======================================================================
-FAIL: test_eval_1 (__main__.TestCollatz)
+% coverage3 run --branch TestCollatz.py >  TestCollatz.out 2>&1
+
+
+
+% coverage3 report -m                   >> TestCollatz.out
+
+
+
+% cat TestCollatz.out
+.......
 ----------------------------------------------------------------------
-Traceback (most recent call last):
-  File "TestCollatz.py", line 47, in test_eval_1
-    self.assertEqual(v, 20)
-AssertionError: 1 != 20
+Ran 7 tests in 0.001s
 
-======================================================================
-FAIL: test_eval_2 (__main__.TestCollatz)
-----------------------------------------------------------------------
-Traceback (most recent call last):
-  File "TestCollatz.py", line 51, in test_eval_2
-    self.assertEqual(v, 125)
-AssertionError: 1 != 125
-
-======================================================================
-FAIL: test_eval_3 (__main__.TestCollatz)
-----------------------------------------------------------------------
-Traceback (most recent call last):
-  File "TestCollatz.py", line 55, in test_eval_3
-    self.assertEqual(v, 89)
-AssertionError: 1 != 89
-
-======================================================================
-FAIL: test_eval_4 (__main__.TestCollatz)
-----------------------------------------------------------------------
-Traceback (most recent call last):
-  File "TestCollatz.py", line 59, in test_eval_4
-    self.assertEqual(v, 174)
-AssertionError: 1 != 174
-
-======================================================================
-FAIL: test_solve (__main__.TestCollatz)
-----------------------------------------------------------------------
-Traceback (most recent call last):
-  File "TestCollatz.py", line 78, in test_solve
-    self.assertEqual(w.getvalue(), "1 10 20\n100 200 125\n201 210 89\n900 1000 174\n")
-AssertionError: '1 10 1\n100 200 1\n201 210 1\n900 1000 1\n' != '1 10 20\n100 200 125\n201 210 89\n900 1000 174\n'
-- 1 10 1
-?      ^
-+ 1 10 20
-?      ^^
-- 100 200 1
-+ 100 200 125
-?          ++
-- 201 210 1
-?         ^
-+ 201 210 89
-?         ^^
-- 900 1000 1
-+ 900 1000 174
-?           ++
-
-
-----------------------------------------------------------------------
-Ran 7 tests in 0.004s
-
-FAILED (failures=5)
-
-
-
-% coverage3 report -m
-Name           Stmts   Miss Branch BrMiss  Cover   Missing
-----------------------------------------------------------
+OK
+Name          Stmts   Miss Branch BrMiss  Cover   Missing
+---------------------------------------------------------
 Collatz          18      0      6      0   100%
-TestCollatz      33      1      0      0    97%   86
-----------------------------------------------------------
-TOTAL            51      1      6      0    98%
-"""
+TestCollatz      32      1      0      0    97%   78
+---------------------------------------------------------
+TOTAL            50      1      6      0    98%
